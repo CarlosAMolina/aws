6fe0e4c4bdd5d397929149fd7e047d9254d1487e
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-09-26 17:22:05 +0200

    Fix lambda local logger

--- lambda/local/lambda/src/lambda_handler.py
+++ lambda/local/lambda/src/lambda_handler.py
@@ -2,31 +2,31 @@ import logging
 import pathlib
 
 
-"""https://realpython.com/python-logging/#using-handlers"""
-# Create a custom logger
-logger = logging.getLogger(__name__)
-# Create handlers
-c_handler = logging.StreamHandler()
-f_handler = logging.FileHandler(str(pathlib.Path.cwd().joinpath("src/lambda-log.log")))
-# TODO fix logger, log.info logs are not created.
-c_handler.setLevel(logging.INFO)
-f_handler.setLevel(logging.INFO)
-
-# Create formatters and add it to handlers
-c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-c_handler.setFormatter(c_format)
-f_handler.setFormatter(f_format)
-# Add handlers to the logger
-logger.addHandler(c_handler)
-logger.addHandler(f_handler)
-logger.info("test")
-logger.warning("test")
+def get_logger():
+    """https://realpython.com/python-logging/#using-handlers"""
+    logging_format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging_level = logging.INFO
+    logging_file_pathname = str(pathlib.Path.cwd().joinpath("src/lambda-log.log"))
+    logging.basicConfig(
+        level=logging_level,
+        format=logging_format,
+    )
+    # Create a custom logger
+    logger = logging.getLogger(__name__)
+    # Create handlers
+    f_handler = logging.FileHandler(logging_file_pathname, mode="w")
+    f_handler.setLevel(logging_level)
+    # Create formatters and add it to handlers
+    f_format = logging.Formatter(logging_format)
+    f_handler.setFormatter(f_format)
+    # Add handlers to the logger
+    logger.addHandler(f_handler)
+    return logger
 
 
 def lambda_handler(event: dict, context):
-    logger.error("Init lambda")
-    logger.info(f"Event: {event}")
+    logger = get_logger()
+    logger.info(f"Init event: {event}")
     response = {"result": "foo"}
     logger.info(f"Output: {response}")
     return response
