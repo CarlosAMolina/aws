0809bd16aa6d26a43ef13a21c54f8b1e8580b160
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-04 03:50:43 +0100

    feat(authenticate): add error InvalidAccessKeyId

--- authenticate/boto3/src/errors.py
+++ authenticate/boto3/src/errors.py
@@ -18,9 +18,34 @@ class _NoAwsCredentials:
 
     def _run_s3_request(self):
         session = boto3.Session()
-        s3_client = session.client("s3", endpoint_url=os.getenv("AWS_ENDPOINT"))
+        s3_client = session.client("s3")
+        s3_client.list_objects_v2(Bucket="foo", Prefix="bar", Delimiter="/")
+
+
+class _IncorrectAwsCredentials:
+    def run(self):
+        self._set_aws_credentials()
+        try:
+            self._run_s3_request()
+        except ClientError as exception:
+            assert exception.response["Error"]["Code"] == "InvalidAccessKeyId"
+
+    def _set_aws_credentials(self):
+        """ "
+        http://docs.getmoto.org/en/latest/docs/getting_started.html#how-do-i-avoid-tests-from-mutating-my-real-infrastructure
+        """
+        os.environ["AWS_ACCESS_KEY_ID"] = "foo"
+        os.environ["AWS_SECRET_ACCESS_KEY"] = "foo"
+        os.environ["AWS_SECURITY_TOKEN"] = "foo"
+        os.environ["AWS_SESSION_TOKEN"] = "foo"
+        os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+    def _run_s3_request(self):
+        session = boto3.Session()
+        s3_client = session.client("s3")
         s3_client.list_objects_v2(Bucket="foo", Prefix="bar", Delimiter="/")
 
 
 if __name__ == "__main__":
     _NoAwsCredentials().run()
+    _IncorrectAwsCredentials().run()
