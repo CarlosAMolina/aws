c7e691024db1e8e00db200d16e45a61568ba1c05
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-06 06:54:33 +0100

    feat(s3 authentication): improve assertion

--- authenticate/boto3/src/errors.py
+++ authenticate/boto3/src/errors.py
@@ -7,10 +7,12 @@ from botocore.exceptions import ClientError
 class _NoAwsCredentials:
     def run(self):
         self._drop_aws_environ_values()
+        error_code = None
         try: 
             self._run_s3_request()
         except ClientError as exception:
-            assert exception.response["Error"]["Code"] == "AccessDenied"
+            error_code = exception.response["Error"]["Code"]
+        _assert_error_code_has_expected_value(error_code, "AccessDenied")
 
     def _drop_aws_environ_values(self):
         for environ_key in ("AWS_SECRET_ACCESS_KEY", "AWS_SECURITY_TOKEN", "AWS_SESSION_TOKEN", "AWS_DEFAULT_REGION"):
@@ -24,11 +26,13 @@ class _NoAwsCredentials:
 
 class _IncorrectAwsCredentials:
     def run(self):
+        error_code = None
         self._set_aws_credentials()
         try:
             self._run_s3_request()
         except ClientError as exception:
-            assert exception.response["Error"]["Code"] == "InvalidAccessKeyId"
+            error_code = exception.response["Error"]["Code"]
+        _assert_error_code_has_expected_value(error_code, "InvalidAccessKeyId")
 
     def _set_aws_credentials(self):
         """ "
@@ -44,6 +48,11 @@ class _IncorrectAwsCredentials:
         session = boto3.Session()
         s3_client = session.client("s3")
         s3_client.list_objects_v2(Bucket="foo", Prefix="bar", Delimiter="/")
+
+
+def _assert_error_code_has_expected_value(error_code: str | None, expected_value: str):
+    if error_code != expected_value:
+        raise ValueError(f"Current value: {error_code}. Expected value: {expected_value}")
 
 
 if __name__ == "__main__":
