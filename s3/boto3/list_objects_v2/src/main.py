34ee35c4bd0c8d2ac65607b059d4bbdea683786d
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-10 04:49:28 +0100

    feat(list_objects_v2): add s3 local server

--- s3/boto3/list_objects_v2/src/main.py
+++ s3/boto3/list_objects_v2/src/main.py
@@ -1,15 +1,20 @@
 import boto3
-import sys
 
-bucket = sys.argv[1]
-path = "tmp"
+from s3_local_server import S3Objects
+from s3_local_server import S3Server
+
+
+bucket = "bucket-test"
+path = "/tmp/"
 
 s3_client = boto3.client("s3")
+
 
 def get_and_show_maximum_response():
     print(f"Init get_and_show_maximum_response")
     response = s3_client.list_objects_v2(Bucket=bucket, Prefix=path, MaxKeys=1000)
     show_response_and_contents(response)
+
 
 def get_and_show_with_start_listing_from():
     print(f"Init get_and_show_with_start_listing_from")
@@ -20,26 +25,38 @@ def get_and_show_with_start_listing_from():
         show_contents(response)
         last_key = response["Contents"][-1]["Key"]
         print(f"last_key={last_key}")
-        response = s3_client.list_objects_v2(Bucket=bucket, Prefix=path, MaxKeys=max_keys, StartAfter=last_key)
+        response = s3_client.list_objects_v2(
+            Bucket=bucket, Prefix=path, MaxKeys=max_keys, StartAfter=last_key
+        )
     print("All data has been retrieved")
+
 
 def has_s3_more_objects_to_retrieve(response: dict) -> bool:
     return response.get("Contents") is not None
+
 
 def show_response_and_contents(response: dict):
     print(response)
     show_contents(response)
 
+
 def show_contents(response: dict):
     contents = response["Contents"]
-    #print(len(contents))
-    #print(contents)
+    # print(len(contents))
+    # print(contents)
     for index, content in enumerate(contents):
         print(index, content)
 
 
+def run():
+    S3Server().start()
+    s3_objects = S3Objects(bucket)
+    s3_objects.create_bucket()
+    s3_objects.upload_files()
+    get_and_show_maximum_response()
+    # get_and_show_with_start_listing_from()
+    S3Server().stop()
+
 
 if __name__ == "__main__":
-    #get_and_show_maximum_response()
-    get_and_show_with_start_listing_from()
-
+    run()
---
+++ s3/boto3/list_objects_v2/src/s3_local_server.py
@@ -0,0 +1,47 @@
+import os
+
+import boto3
+from moto import mock_aws
+
+
+class S3Server:
+    def __init__(self):
+        """http://docs.getmoto.org/en/latest/docs/getting_started.html"""
+        self._set_aws_credentials()
+        self._mock_aws = mock_aws()
+
+    def start(self):
+        self._mock_aws.start()
+
+    def stop(self):
+        self._mock_aws.stop()
+
+    def _set_aws_credentials(self):
+        """ "
+        http://docs.getmoto.org/en/latest/docs/getting_started.html#how-do-i-avoid-tests-from-mutating-my-real-infrastructure
+        """
+        os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+        os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+        os.environ["AWS_SECURITY_TOKEN"] = "testing"
+        os.environ["AWS_SESSION_TOKEN"] = "testing"
+        os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+
+class S3Objects:
+    def __init__(self, bucket_name: str):
+        self._bucket_name = bucket_name
+        self._s3_resource = boto3.resource("s3")
+        self._s3_client = boto3.client("s3")
+
+    def create_bucket(self):
+        bucket = self._s3_resource.Bucket(self._bucket_name)
+        bucket.create()
+
+    def upload_files(self):
+        content = "foo"
+        file_name = "foo.html"
+        prefix = "/tmp/"
+        key = f"{prefix}{file_name}"
+        self._s3_client.put_object(
+            Body=content, Bucket=self._bucket_name, Key=key, ContentType="text/html"
+        )
